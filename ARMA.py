73b02a76e902026b9da02e230a19b9353916b2d1
jslimaye <2016.jalaj.limaye@ves.ac.in>
2019-04-04 12:58:52 +0530

    last commit

--- ARMA.py
+++ ARMA.py
@@ -16,7 +16,9 @@ def fit(y,p,q):
     n = len(y)
        
     e = cal_moving_averages(y)
-
+    print(y)
+    print(type(y))
+    
     if p < q : 
         i = p + 1
     else:
@@ -27,10 +29,13 @@ def fit(y,p,q):
         var = list()
         b = list()
         while (t < i+p+q):
-            v = reverse(y[t-p:t])            
+            v = []
+            v.append(list(reversed(y[t-p:t])))
+            
+            for item in reversed(y[t-p:t]):
+                v.append(item)
             v.append(e[t])
-            v.append(reverse(e[t-q:t]))
-
+            v.append(list(reversed(e[t-q:t])))
             var.append(v)
 
             b.append(y[t])
@@ -41,29 +46,71 @@ def fit(y,p,q):
         b = np.array(b)
         m = np.linalg.solve(a,b)
         models.append(m)
+        i = i+1
 
-    final_model =  dict()
-	ar = list()
-	ma = list()
+    final_model = dict()
+    ar = list()
+    ma = list()
     for j in range(p):
         a = list()
         for i in range(len(models)):
             a.append(models[i][j])
         mean = np.mean(a)
-		ar.append(mean)
-		
-	for j in range(p,q):
+        ar.append(mean)
+
+    for j in range(p,q):
         a = list()
         for i in range(len(models)):
             a.append(models[i][j])
         mean = np.mean(a)
-		ma.append(mean)
-	
-	final_model['ar'] = ar
-	final_model['ma'] = ma
-	
-	
-	return final_model
- 
-def forecast(series,model,n):
-	return
+        ma.append(mean)
+
+    final_model['ar'] = ar
+    final_model['ma'] = ma
+    print(final_model)
+    return final_model 
+
+def fixed_fit(y):
+    models = list()
+    n = len(y) 
+    e = cal_moving_averages(y)
+    
+    models = list()
+    i = 2
+    while i<n:
+        a = list()
+        b = list()
+        coeff = list()
+        
+        a.append(y[i-1])
+        a.append(e[i-1])
+        
+        b.append(y[i-2])
+        b.append(e[i-2])
+        
+        c = list()
+        c.append(y[i])
+        c.append(y[i-1])
+        
+        coeff.append(a)
+        coeff.append(b)
+        
+        m = np.linalg.solve(coeff,c)
+        #print(m)
+        models.append(list(m))
+        i = i+1
+    
+    ar = 0
+    ma = 0
+    
+    a = list()
+    for i in range(len(models)):
+        a.append(models[i][0])
+    ar = np.mean(a)
+
+    a = list()
+    for i in range(len(models)):
+        a.append(models[i][1])
+    ma = np.mean(a)
+
+    return ar,ma
